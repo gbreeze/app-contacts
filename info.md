1d8e4a1414023547fdc73be2460261621cfd4284
Georg Perhofer <georg.perhofer@aimsoftware.com>
2017-01-19 13:11:52 +0100

    added move handler

--- info.md
+++ info.md
@@ -9,3 +9,11 @@ http://aurelia.io/hub.html#/doc/article/aurelia/framework/latest/contact-manager
 
 
 
+## sortable
+
+https://github.com/eriklieben/aurelia-sortablejs
+https://github.com/rubaxa/Sortable
+
+demo
+http://jsbin.com/luxero/2/edit?html,js,output
+
--- src/contact-list.ts
+++ src/contact-list.ts
@@ -2,15 +2,16 @@ import { EventAggregator } from 'aurelia-event-aggregator';
 import { WebAPI } from './web-api';
 import { ContactUpdated, ContactViewed } from './messages';
 import { inject } from 'aurelia-framework';
-import Sortable = require('sortablejs');
-
 
 @inject(WebAPI, EventAggregator)
 export class ContactList {
   contacts;
   selectedId = 0;
 
-  options = {}
+  options = {
+    handle: '.move',
+    animation: 150
+  }
 
   constructor(private api: WebAPI, ea: EventAggregator) {
     ea.subscribe(ContactViewed, msg => this.select(msg.contact));
@@ -19,10 +20,6 @@ export class ContactList {
       let found = this.contacts.find(x => x.id == id);
       Object.assign(found, msg.contact);
     });
-  }
-
-  attached() {
-    //Sortable.create(document.querySelector(".list-group"), {})
   }
 
   created() {
@@ -34,9 +31,26 @@ export class ContactList {
     return true;
   }
 
-
   func(customEvent: CustomEvent) {
     let event = customEvent.detail;
     console.log("event", event);
+
+    //// If item isn't being dropped into its original place
+    if (event.newIndex != event.oldIndex) {
+      //// https://ilikekillnerds.com/2015/09/aurelia-and-dragdrop-using-sortable/
+      this.swapArrayElements(this.contacts, event.newIndex, event.oldIndex)
+    }
+
+    console.log(this.contacts)
+
   }
+
+  swapArrayElements(theArray: Array<any>, a: number, b: number) {
+    let temp = theArray[a];
+    theArray[a] = theArray[b];
+    theArray[b] = temp;
+  }
+
+
 }
+
