8b41011c51cc4315ec2cd3e1aec738399cd1fefe
Georg Perhofer <georg.perhofer@aimsoftware.com>
2017-01-30 17:30:15 +0100

    changed css to scss

--- aurelia_project/tasks/process-css.ts
+++ aurelia_project/tasks/process-css.ts
@@ -1,10 +1,12 @@
 import * as gulp from 'gulp';
 import * as changedInPlace from 'gulp-changed-in-place';
 import * as project from '../aurelia.json';
+import * as sass from 'gulp-sass';
 import {build} from 'aurelia-cli';
 
 export default function processCSS() {
   return gulp.src(project.cssProcessor.source)
-    .pipe(changedInPlace({firstPass:true}))
+    //.pipe(changedInPlace({firstPass:true}))
+    .pipe(sass().on('error', sass.logError))
     .pipe(build.bundle());
 };
